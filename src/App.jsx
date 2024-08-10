230e65a97b1c56ba5db53d7d8d41a7cf083bb1b2
Stefan Jaroni <sjaroni@outlook.de>
2024-08-10 22:57:13 +0200

    feat: add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Product from './components/product';
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return <React.Fragment>
+      <Navbar/>
+      <div className="product-container">
+        <Product/>
+        <Product/>
+        <Product/>
+      </div>
+    </React.Fragment>
+    ;
+  }
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Product from './components/product';
+import './index.css'
 
-// const element = <h1>Hello World</h1>;
-// ReactDom.render(element, document.getElementById('root'));
-
-import Navbar from './components/navbar';
-// ReactDom.render(<Navbar/>, document.getElementById('root'));
-ReactDom.render(<Product/>, document.getElementById('root'));
+import App from './App';
+ReactDom.render(<App/>, document.getElementById('root'));
