9296242a85c76e88074cde031cdf3e973e0d306c
Stefan Jaroni <sjaroni@outlook.de>
2024-08-10 23:26:39 +0200

    feat: add component variables as inputs

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,28 @@ import Navbar from './components/navbar';
 import Product from './components/product';
 
 class App extends Component {
-  state = {  } 
-  render() { 
-    return <React.Fragment>
-      <Navbar/>
-      <div className="product-container">
-        <Product/>
-        <Product/>
-        <Product/>
-      </div>
-    </React.Fragment>
-    ;
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className='product-container'>
+          <Product
+            title='Sneaker'
+            image='sneaker.jpg'
+            description='Komfortabel atmungsaktiv, Herrenschuhe Material perforiertes Design, atmungsaktiv, bequem.'
+          />
+          <Product
+            title='Jeans'
+            image='jeans.jpg'
+            description='Moderne Denim Jeans in verschiedenen Waschungen - Hochwertige Verarbeitung'
+          />
+          <Product title='Cap' image='cap.png' description="Casual Look - Die perfekte Cap wenn es mal etwas schlichter sein soll"/>
+          <Product title='Shirt' image='shirt.png' description="Das T-Shirt hat im Vergleich zu einem normalen T-Shirt ..."/>
+        </div>
+      </React.Fragment>
+    );
   }
 }
- 
+
 export default App;
