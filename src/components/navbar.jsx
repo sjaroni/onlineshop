eaebce39f13a7c373204c419b69b0bb4515b38a0
Stefan Jaroni <sjaroni@outlook.de>
2024-08-10 22:23:51 +0200

    style: add bootstrap navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-  state = {  } 
-  render() { 
-    return <nav>Navigation</nav>;
+  state = {};
+  render() {
+    return (
+      <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Navbar</a>
+      </div>
+    </nav>
+    );
   }
 }
- 
+
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // const element = <h1>Hello World</h1>;
 // ReactDom.render(element, document.getElementById('root'));
