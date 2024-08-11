0c6c976729274bff0d7e118fe8b96ab807bfc792
Stefan Jaroni <sjaroni@outlook.de>
2024-08-11 19:58:44 +0200

    feat: update to react 18

--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
-  state = {};
+  state = {
+    items: [
+    {
+      amount: 1,
+      name: 'Sneaker',
+      price: 149.95
+    },
+    {
+      amount: 1,
+      name: 'Shirt',
+      price: 39.99
+    },
+  ]
+  };
+
+
   render() {
     return (
       <div className="shopping-cart">
         <h2>Warenkorb</h2>
+
+        { this.state.items.map(item => <div key={item.name}>{ item.amount} { item.name} </div>)}
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 import App from './App';
-ReactDom.render(<App/>, document.getElementById('root'));
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<App></App>);
