6a9a13413e467939393d5ff3c6968acc018afbb8
Stefan Jaroni <sjaroni@outlook.de>
2024-08-11 19:43:50 +0200

    feat: add shopping-cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Product from './components/product';
+import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
   state = {};
@@ -8,19 +9,30 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar />
-        <div className='product-container'>
-          <Product
-            title='Sneaker'
-            image='sneaker.jpg'
-            description='Komfortabel atmungsaktiv, Herrenschuhe Material perforiertes Design, atmungsaktiv, bequem.'
-          />
-          <Product
-            title='Jeans'
-            image='jeans.jpg'
-            description='Moderne Denim Jeans in verschiedenen Waschungen - Hochwertige Verarbeitung'
-          />
-          <Product title='Cap' image='cap.png' description="Casual Look - Die perfekte Cap wenn es mal etwas schlichter sein soll"/>
-          <Product title='Shirt' image='shirt.png' description="Das T-Shirt hat im Vergleich zu einem normalen T-Shirt ..."/>
+        <div className='main-container'>
+          <div className='product-container'>
+            <Product
+              title='Sneaker'
+              image='sneaker.jpg'
+              description='Komfortabel atmungsaktiv, Herrenschuhe Material perforiertes Design, atmungsaktiv, bequem.'
+            />
+            <Product
+              title='Jeans'
+              image='jeans.jpg'
+              description='Moderne Denim Jeans in verschiedenen Waschungen - Hochwertige Verarbeitung'
+            />
+            <Product
+              title='Cap'
+              image='cap.png'
+              description='Casual Look - Die perfekte Cap wenn es mal etwas schlichter sein soll'
+            />
+            <Product
+              title='Shirt'
+              image='shirt.png'
+              description='Das T-Shirt hat im Vergleich zu einem normalen T-Shirt ...'
+            />
+          </div>
+          <ShoppingCart/>
         </div>
       </React.Fragment>
     );
