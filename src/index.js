35b8926d9f4a1733fa9a71a3ccaf2149c55d4169
Stefan Jaroni <sjaroni@outlook.de>
2024-08-12 13:12:05 +0200

    fix: fix errors from v17 syntax

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
+import * as ReactDomClient from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 import App from './App';
 
-const root = ReactDom.createRoot(document.getElementById('root'));
+const root = ReactDomClient.createRoot(document.getElementById('root'));
 root.render(<App></App>);
